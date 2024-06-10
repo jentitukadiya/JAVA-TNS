b1993d43b083e7045583a0bb1e4c36226b50aec3
jentitukadiya <149855551+jentitukadiya@users.noreply.github.com>
2024-06-10 07:56:26 +0530

    Add files via upload

---
+++ Day11_Animal/Main.java
@@ -0,0 +1,13 @@
+package Day11_Animal;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cats c = new Cats();
+        Dogs d = new Dogs();
+
+        c.cat();
+        d.dog();
+    }
+
+}
---
+++ Day11_FunctionInterface/Greeting.java
@@ -0,0 +1,6 @@
+package Day11_FunctionInterface;
+
+@FunctionalInterface
+public interface Greeting {
+    public void fun();
+}
